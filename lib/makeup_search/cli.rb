1b0b36baca35426c835a89639e0ea247398a895b
Meghan Flynn <mflynn333@gmail.com>
2020-02-05 00:48:30 +0000

    Tried to get my executable file to work

--- lib/makeup_search/cli.rb
+++ lib/makeup_search/cli.rb
@@ -1,5 +1,7 @@
 class MakeupSearch::CLI 
+  
   def call 
     puts 'Hello gorgeous!'
   end 
+  
 end 
--- lib/makeup_search/environment.rb
+++ lib/makeup_search/environment.rb
@@ -1,9 +1,10 @@
 require_relative "./makeup_search/version"
 require_relative "./makeup_search/cli"
 
-module MakeupSearch
-    class Error < StandardError; end
+#module MakeupSearch
+    #class Error < StandardError; end
     # Your code goes here...
-  end
-end
+#end
+
+puts "environment file"
 
