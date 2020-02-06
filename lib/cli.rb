d8a042904afe845d5fa85958d9208e3c4d0939b0
Meghan Flynn <mflynn333@gmail.com>
2020-02-06 00:04:43 +0000

    modified cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,9 @@
 class MakeupSearch::CLI
   
   def call 
-    puts 'Hello gorgeous!'
+    puts 'Hello gorgeous! What are you looking for today?'
+    puts 'To see makeup products, enter products'
+    puts 'To ext, enter exit'
   end 
   
 end 
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,7 @@
 require_relative './makeup_search/version'
 require_relative './cli'
+require_relative './api'
+require_relative './makeup'
 
 module MakeupSearch
     class Error < StandardError; end
