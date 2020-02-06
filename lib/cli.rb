7d7426bd700e3d4872e37f21a783cbe726c868f5
Meghan Flynn <mflynn333@gmail.com>
2020-02-06 00:26:33 +0000

    modified cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,7 +3,26 @@ class MakeupSearch::CLI
   def call 
     puts 'Hello gorgeous! What are you looking for today?'
     puts 'To see makeup products, enter products'
-    puts 'To ext, enter exit'
+    puts 'To exit, enter exit'
+    input_menu
+  end 
+  
+  def input_menu
+    input = gets.strip.downcase
+    
+    if input == "products"
+      puts "products"
+    else if input == "exit"
+      puts "exit"
+    else 
+      invalid_entry
+      end 
+    end 
+  end 
+  
+  def invalid_entry 
+    puts "invalid entry, try again"
+      input_menu
   end 
   
 end 
