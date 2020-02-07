913eff4ffb84fb62a124871a71394719183bb90d
Meghan Flynn <mflynn333@gmail.com>
2020-02-07 04:21:07 +0000

    modified makeup.rb

--- lib/api.rb
+++ lib/api.rb
@@ -2,8 +2,10 @@ class MakeupSearch::API
   
   def self.get_data 
     response = RestClient.get("https://makeup-api.herokuapp.com/api/v1/products.json")
-    product_type_array = JSON.parse(response.body)["name"]
-    product_type_array.each do |product|
+    product_array = JSON.parse(response.body) #[]
+    
+    
+    product_array.each do |product|
       Makeup.new(product)
     end 
   end 
