d013b9f90f43db03a79c58eb9f7bf5fa1202913a
Meghan Flynn <mflynn333@gmail.com>
2020-02-07 23:52:21 +0000

    modified makeup file

--- lib/makeup.rb
+++ lib/makeup.rb
@@ -1,30 +1,5 @@
-class Makeup
+class MakeupSearch::Makeup
   
-  @@all = []
-  
-  attr_accessor :name, :brand, :product_type, :category, :description
-  
-  #def initialize(name, brand, product_type, category, description)
-    #@name = name   #lookup metaprogramming to make this easier 
-    #@brand = brand 
-    #@product_type = product_type 
-    #@category = category
-    #@description = description
-  #end 
-  
-  def initialize(hash)
-    hash.each.do |key, value|
-      self.send(("#{key}="), value) if self.respond_to?(("#{key}="))
-    end 
-    save
-  end 
-  
-  def save 
-    @@all << self
-  end 
-  
-  def self.all 
-    @@all
-  end 
+  #attr_accessor :name, :brand, :product_type, :category, :description
   
 end 
