d47daa4d7de779ddc0696bbfbacc95ecc0dc66d0
Meghan Flynn <mflynn333@gmail.com>
2020-02-07 23:33:30 +0000

    modified environment file

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,7 @@
 require_relative './makeup_search/version'
 require_relative './cli'
 require_relative './api'
-require_relative './makeup.rb'
+require_relative './makeup'
 
 require 'rest-client'
 require 'json'
@@ -14,3 +14,12 @@ end
 
 puts "environment file"
 
+
+
+
+
+
+
+url = 'https://api.spotify.com/v1/search?type=artist&q=tycho'
+response = RestClient.get(url)
+JSON.parse(response)
