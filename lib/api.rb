67bb82c61484a1eb441e36d2a80060ec5823a9f4
Meghan Flynn <mflynn333@gmail.com>
2020-02-07 00:35:12 +0000

    modified api.rb and environment.rb

--- lib/api.rb
+++ lib/api.rb
@@ -4,5 +4,8 @@ class MakeupSearch::API
     response = RestClient.get("https://makeup-api.herokuapp.com/api/v1/products.json")
     product_type_array = JSON.parse(response.body)["results"]
     product_type_array.each do |product|
-      Product.new(Product)
+      Makeup.new(product)
+    end 
+  end 
+  
 end 
--- lib/environment.rb
+++ lib/environment.rb
@@ -3,6 +3,9 @@ require_relative './cli'
 require_relative './api'
 require_relative './makeup'
 
+require 'rest-client'
+require 'json'
+
 module MakeupSearch
     class Error < StandardError; end
     # Your code goes here...
