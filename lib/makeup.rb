6cacea7e880d934a1762b50f75d253a9fe293c31
Meghan Flynn <mflynn333@gmail.com>
2020-02-07 04:11:10 +0000

    modified makeup.rb

--- lib/makeup.rb
+++ lib/makeup.rb
@@ -1,4 +1,13 @@
 class Makeup
   
   attr_accessor :name, :brand, :product_type, :category, :description
+  
+  def initialize(name, brand, product_type, category, description)
+    @name = name   #lookup metaprogramming to make this easier 
+    @brand = brand 
+    @product_type = product_type 
+    @category = category
+    @description = description
+  end 
+  
 end 
