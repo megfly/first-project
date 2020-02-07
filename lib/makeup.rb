19a6574a529f2bca3d2de3e9f6d0161455ee9bc0
Meghan Flynn <mflynn333@gmail.com>
2020-02-07 04:14:48 +0000

    modified makeup.rb

--- lib/makeup.rb
+++ lib/makeup.rb
@@ -1,13 +1,30 @@
 class Makeup
   
+  @@all = []
+  
   attr_accessor :name, :brand, :product_type, :category, :description
   
-  def initialize(name, brand, product_type, category, description)
-    @name = name   #lookup metaprogramming to make this easier 
-    @brand = brand 
-    @product_type = product_type 
-    @category = category
-    @description = description
+  #def initialize(name, brand, product_type, category, description)
+    #@name = name   #lookup metaprogramming to make this easier 
+    #@brand = brand 
+    #@product_type = product_type 
+    #@category = category
+    #@description = description
+  #end 
+  
+  def initialize(hash)
+    hash.each.do |key, value|
+      self.send("key=", value)
+    end 
+    save
+  end 
+  
+  def save 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
   end 
   
 end 
