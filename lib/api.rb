48e57891950c71aef62d0ae857250c04cbaf4521
Meghan Flynn <mflynn333@gmail.com>
2020-02-08 00:41:12 +0000

    modified api file

--- lib/api.rb
+++ lib/api.rb
@@ -1,17 +1,15 @@
 class MakeupSearch::API 
-  attr_accessor :name, :brand, :product_type, :category, :description
   
   def self.get_data 
     binding.pry
     url = 'https://makeup-api.herokuapp.com/api/v1/products.json'
     response = RestClient.get(url)
     JSON.parse(response)
-  end 
-    
     
     #product_array.each do |product|
       #Makeup.new(product)
     #end 
-  #end 
+  end 
+    
   
 end 
