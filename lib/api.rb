3c20055d0bb1b1883a133d83a739f02d2f3ee979
Meghan Flynn <mflynn333@gmail.com>
2020-02-13 00:58:04 +0000

    modified api file

--- lib/api.rb
+++ lib/api.rb
@@ -7,16 +7,16 @@ class MakeupSearch::API
   def self.get_data 
       url = 'https://makeup-api.herokuapp.com/api/v1/products.json'
       response = HTTParty.get(url)
-      #makeup_arr = JSON.parse(response.body)
-      
-      
       response.each do |makeup|
         #makeup_object = Makeup.new
         binding.pry
         
-        name = makeup_arr["name"]
-        product_type = makeup_arr["product_type"]
-        description = makeup_arr["description"]
+        name = makeup["name"]
+        brand = makeup["brand"]
+        product_type = makeup["product_type"]
+        description = makeup["description"]
+        
+        MakeupSearch::Makeup.new(name, brand, product_type, description)
         
     end 
     
@@ -25,5 +25,6 @@ end
 
 
 MakeupSearch::API.get_data
-
+binding.pry
+true 
 
--- lib/makeup.rb
+++ lib/makeup.rb
@@ -1,12 +1,13 @@
 require 'pry'
 class MakeupSearch::Makeup
   
-  attr_accessor :name, :product_type, :description
+  attr_accessor :name, :brand, :product_type, :description
   
   @@all = [] 
   
   def initialize(name, brand, product_type, description)
     @name = name 
+    @brand = brand
     @product_type = product_type 
     @description = description
     @@all << self
