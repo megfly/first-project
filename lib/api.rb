3ec44bc904ebdbc7365b47f73bd8daffa2822dad
Meghan Flynn <mflynn333@gmail.com>
2020-02-13 00:46:41 +0000

    cleaned up api class

--- lib/api.rb
+++ lib/api.rb
@@ -4,21 +4,15 @@ require_relative './makeup_search/version'
 
 class MakeupSearch::API 
   
-  attr_accessor :name, :product_type, :description
-  
-  def initialize(name)
-    @name = name
-    @product_type = product_type 
-    @description = description
-  end 
-  
   def self.get_data 
       url = 'https://makeup-api.herokuapp.com/api/v1/products.json'
       response = HTTParty.get(url)
-      makeup_arr = JSON.parse(response.body)
+      #makeup_arr = JSON.parse(response.body)
       
-      makeup_arr.each do |makeup|
+      
+      response.each do |makeup|
         #makeup_object = Makeup.new
+        binding.pry
         
         name = makeup_arr["name"]
         product_type = makeup_arr["product_type"]
